0dadf527323b9425d9d295f4653d1f26558cb325
Paul <paul.salina@gmail.com>
2017-03-20 18:32:47 -0400

    finished the track but hit a wall on the bridge with this model

--- utils.py
+++ utils.py
@@ -73,7 +73,7 @@ def get_image_steering(root_path):
     return images, measurements
 
 # the generator will also use the left and right camera images and apply a steering factor
-def generator(root_path, samples, batch_size=32, aug=False, num_aug=5):
+def generator(root_path, samples, batch_size=12, aug=False, num_aug=5):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
@@ -104,15 +104,15 @@ def generator(root_path, samples, batch_size=32, aug=False, num_aug=5):
                     for aug_i  in range(num_aug):
                         x, y = None, None
                         keep_pr = 0
-                        while keep_pr == 0:
-                            x, y = preprocess_aug(image, angle)
+#                        while keep_pr == 0:
+                        x, y = preprocess_aug(image, angle)
 
-                            if abs(y) < .1:
-                                pr_val = np.random.uniform()
-                                if pr_val >  1:
-                                    keep_pr = 1
-                            else:
-                                keep_pr = 1
+                        #    if abs(y) < .1:
+                        #        pr_val = np.random.uniform()
+                        #        if pr_val >  1:
+                        #            keep_pr = 1
+                        #    else:
+                        #        keep_pr = 1
                         
                         images.append(x)
                         angles.append(y)
@@ -218,7 +218,7 @@ def trans_image(image,steer,trans_range):
 
     # Translation
     tr_x = trans_range * np.random.uniform() - trans_range / 2
-    steer_ang = steer + tr_x /trans_range * 2 * .2
+    steer_ang = steer + tr_x  / trans_range * 2 * .6
     tr_y = 40 * np.random.uniform() - 40 / 2
     
     #tr_y = 0
